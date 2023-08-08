37d1cae996cc79694d4ebc0aa36029914a330a64
Camila Santos <camila8925@hotmail.com>
2023-08-08 17:13:08 +0000

    Atualizando

---
+++ src/main/java/com/example/App1.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class App1 {
+
+public static void main(String[] args) {
+    System.out.println("Minha aplicação Java\n");
+
+    byte idade = 35;
+    double peso = 62;
+    System.out.println("Idade é " + idade + " " + "Peso é " + peso);
+
+    char sexo = 'F';
+    System.out.println("Sexo é " + sexo);
+
+    boolean necessidadesEspeciais = false;
+    System.out.println("Tem necessidades especiais:" + necessidadesEspeciais);
+
+}
+
+}
