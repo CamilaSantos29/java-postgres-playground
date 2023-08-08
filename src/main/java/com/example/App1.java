5732371ec4d3290e511e72b44e2e05a65cf9e907
Camila Santos <camila8925@hotmail.com>
2023-08-08 20:02:56 +0000

    Update README.md

---
+++ src/main/java/com/example/App1.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class App1 {
+    
+    public static void main(String[] args){
+        System.out.println();
+        System.out.println();
+        System.out.println("Minha aplicação Java\n");
+
+        //tipo nomeDaVariavel ;
+        int idade = 10, peso = 80;
+        System.out.println("idade é " + idade + "Peso é"  +  peso );
+
+    }
+}
---
+++ src/main/java/com/example/AppArrays.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class AppArrays {
+
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+
+        double vetor [] = {10, 20, 30};
+        System.out.println(Arrays.toString(vetor));
+
+        System.out.println("Valor da posicão 0:" + vetor [0]);
+        System.out.println("Valor da posicão 1:" + vetor [1]);
+        System.out.println("Valor da posicão 2:" + vetor [2]);
+
+        int i = 10;
+        double d = i;
+        System.out.println(d);
+
+        int vetor3[] = new int[4];
+        vetor3[0] = 100;
+        vetor3[1] = 200;
+        vetor3[2] = 300;
+        vetor3[3] = 400;
+
+        System.out.println(Arrays.toString(vetor3));
+    }
+    
+}
+//
